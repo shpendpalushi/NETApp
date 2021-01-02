b07be08fc0394f561511f6060beed24bb94b7209
Shpend Palushi <shpend.palushi@softsolution.al>
2021-01-02 22:31:46 +0100

    Postgres connection

--- API/Controllers/WeatherForecastController.cs
+++ API/Controllers/WeatherForecastController.cs
@@ -31,7 +31,8 @@ namespace API.Controllers
             {
                 Date = DateTime.Now.AddDays(index),
                 TemperatureC = rng.Next(-20, 55),
-                Summary = Summaries[rng.Next(Summaries.Length)]
+                Summary = Summaries[rng.Next(Summaries.Length)],
+                Greeting = "Hello from " + rng.Next(Summaries.Length)
             })
             .ToArray();
         }
